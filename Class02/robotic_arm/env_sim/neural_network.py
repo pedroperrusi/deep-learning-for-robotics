aa99e180001814272cea71dbef201b12d162e989
Pedro Henrique Suruagy Perrusi <suruagyperrusi@etu.unistra.fr>
2020-12-03 20:40:41 +0100

    Added droupout at the end of each FC layer

--- Class02/robotic_arm/env_sim/neural_network.py
+++ Class02/robotic_arm/env_sim/neural_network.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 '''Feed Forward Neural Network For Regression'''
 
@@ -16,16 +15,16 @@ class FullyConnectedNetwork(nn.Module):
         self.h_3 = nn.Linear(num_hidden_neurons[2], num_hidden_neurons[3])
         self.h_4 = nn.Linear(num_hidden_neurons[3], num_hidden_neurons[4])
 
-        self.drop = dropout_rte
+        self.drop = nn.Dropout(dropout_rte)
 
     def forward(self, x):
-        out_0 = F.tanh(self.h_0(x))
-        out_1 = F.tanh(self.h_1(out_0))
-        out_2 = F.tanh(self.h_2(out_1))
-        out_3 = F.tanh(self.h_3(out_2))
-        out = F.tanh(self.h_4(out_3))
+        out_0 = self.drop(torch.tanh(self.h_0(x)))
+        out_1 = self.drop(torch.tanh(self.h_1(out_0)))
+        out_2 = self.drop(torch.tanh(self.h_2(out_1)))
+        out_3 = self.drop(torch.tanh(self.h_3(out_2)))
+        out_4 = self.drop(torch.tanh(self.h_4(out_3)))
 
         ###### Using The Defined Layers and F.tanh As The Nonlinear Function Between Layers Define Forward Function ######
 
-        return out
+        return out_4
 #################################################################################################
